5e0d19e648a4ffb2238aeea0a4089ef6f70ae3a4
Patrick Ehrler <hallo@patrickehrler.de>
2020-12-16 17:49:07 +0100

    rfe implementation + dataset import

--- comparison_algorithms.py
+++ comparison_algorithms.py
@@ -1,7 +1,19 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+
 def sfs():
     print('sfs')
 
-def rfe():
-    print('rfe')
 
-# TODO implement algorithms
+def rfe(data, target, n_features=10, step_size=1, estimator=LinearRegression()):
+    rfe_selection = RFE(estimator=estimator,
+                        n_features_to_select=n_features,
+                        step=step_size)
+    
+    rfe_selection.fit(data, target)
+
+    return rfe_selection.support_
+
+
+
+# TODO implement more algorithms
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import sys
 from sklearn.datasets import fetch_openml
+import pandas as pd
+import numpy as np
+
+from comparison_algorithms import sfs, rfe
 
 print(sys.argv)
 
@@ -13,6 +17,12 @@ comparison_approaches = {
 }
 
 # import dataset
-dataset = fetch_openml('duke-breast-cancer', return_X_y=False) # https://www.openml.org/d/1434
+# IMPORTANT: classification datasets must have numeric classes
+# currently basic dataset is used to have a good performance for development
+X, y = fetch_openml('pendigits', return_X_y=True, as_frame=True) # https://www.openml.org/d/1106
+print('Dataset downloaded')
+res = rfe(data=X, target=y, n_features=5)
+print(res)
+
 
 # TODO: call algorithms using dataset
