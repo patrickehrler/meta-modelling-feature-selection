80f6e30b52bc15b1bf04cfde47c420725f631caa
Patrick Ehrler <hallo@patrickehrler.de>
2021-03-29 12:38:07 +0200

    Move multiprocessing outside datasets in iter experiment

--- iter_experiment.py
+++ iter_experiment.py
@@ -17,16 +17,16 @@ def experiment_bayesian_iter_performance():
                 - Testing score is the score on the test data based on the n highest features (of the final vector)
     """
 
+    # create multiprocess pool
+    pool = mp.Pool(processes=config.n_processes)
+    mp_results = []
+
     for task, dataset in config.data_ids.items():
         for dataset_id, flag in dataset.items():
             if flag == True:
                 # import dataset
                 data, target = fetch_openml(
                     data_id=dataset_id, return_X_y=True, as_frame=True)
-
-                # create multiprocess pool
-                pool = mp.Pool(processes=config.n_processes)
-                mp_results = []
 
                 # use kfold cross validation
                 kf = KFold(n_splits=config.n_splits, shuffle=True).split(data)
@@ -68,41 +68,43 @@ def experiment_bayesian_iter_performance():
                                                     mp_results.append((dataset_id, estimator, metric, learning_method, "-", discretization_method, acq, n_features, test_index, pool.apply_async(
                                                         skopt, [], {"data":data.loc[train_index], "target":target.loc[train_index], "n_features":n_features, "kernel":None, "learning_method":learning_method, "discretization_method":discretization_method, "estimator":estimator, "metric":metric, "acq_func":acq, "n_calls":config.max_calls, "intermediate_results":True, "penalty_weight":0, "cross_validation":config.n_splits_bay_opt, "acq_optimizer":acq_optimizer, "n_convergence":None, "n_acq_points":config.n_acq_points})))
 
-                # get finished tasks (display tqdm progressbar)
-                results = [tuple(r[0:9]) + tuple([r[9].get()]) for r in tqdm(mp_results)]
-                pool.close()
-                pool.join()
+    # get finished tasks (display tqdm progressbar)
+    results = [tuple(r[0:9]) + tuple([r[9].get()]) for r in tqdm(mp_results)]
+    pool.close()
+    pool.join()
 
-                # store in pandas dataframe
-                list_columns = ["Dataset ID", "Estimator", "Metric", "Learning Method", "Kernel", "Discretization Method", "Acquisition Function", "Number Features", "Iteration Steps"]
+    # store in pandas dataframe
+    list_columns = ["Dataset ID", "Estimator", "Metric", "Learning Method", "Kernel", "Discretization Method", "Acquisition Function", "Number Features", "Iteration Steps"]
 
-                # extract data and create dataframe
-                df = []
-                for dataset_id, estimator, metric, learning_method, kernel, discretization_method, acq, n_features, test_index, (_, vector_list, fun_list) in results:
-                    vector_list = vector_list
-                    current_max_training_score = 0
-                    current_max_test_score = 0
-                    max_vector = []
-                    for n_calls in range(0,len(vector_list),1):
-                        # if new maximum is found at iteration step, add to result dataframe
-                        training_score = fun_list[n_calls]
-                        if current_max_training_score < training_score:
-                            current_max_training_score = training_score
-                            # calculate current max feature vector
-                            if n_features is None:
-                                max_vector = vector_list[n_calls]
-                            else:
-                                max_vector = discretize(vector_list[n_calls], "n_highest", n_features)
-                            # calculate test score based on vector that has max training score
-                            current_max_test_score = get_score(data.loc[train_index], data.loc[test_index], target.loc[train_index], target.loc[test_index], max_vector, estimator, metric)
-                        df.append([dataset_id, estimator, metric, learning_method, kernel, discretization_method, acq, n_features, n_calls, current_max_training_score, current_max_test_score])
-
-                df_result = pd.DataFrame(df, columns=list_columns+["Training Score", "Testing Score"])
-                df_result_grouped = df_result.groupby(list_columns, as_index=False).agg(
-                    {"Training Score": ["mean"], "Testing Score": ["mean"]})
-                df_result_grouped_ordered = df_result_grouped.sort_values(by=["Dataset ID", "Estimator", "Metric", "Learning Method", "Kernel", "Discretization Method", "Acquisition Function", "Number Features"], ascending=True) 
-                df_result_grouped_ordered.to_csv(
-                    "results/iteration_number_experiment/bay_opt_iterations_" + str(dataset_id) + ".csv", index=False)
+    # extract data and create dataframe
+    result_array = []
+    for dataset_id, estimator, metric, learning_method, kernel, discretization_method, acq, n_features, test_index, (_, vector_list, fun_list) in results:
+        vector_list = vector_list
+        current_max_training_score = 0
+        current_max_test_score = 0
+        max_vector = []
+        for n_calls in range(0,len(vector_list),1):
+            # if new maximum is found at iteration step, add to result dataframe
+            training_score = fun_list[n_calls]
+            if current_max_training_score < training_score:
+                current_max_training_score = training_score
+                # calculate current max feature vector
+                if n_features is None:
+                    max_vector = vector_list[n_calls]
+                else:
+                    max_vector = discretize(vector_list[n_calls], "n_highest", n_features)
+                # calculate test score based on vector that has max training score
+                current_max_test_score = get_score(data.loc[train_index], data.loc[test_index], target.loc[train_index], target.loc[test_index], max_vector, estimator, metric)
+            result_array.append([dataset_id, estimator, metric, learning_method, kernel, discretization_method, acq, n_features, n_calls, current_max_training_score, current_max_test_score])
+    df_result = pd.DataFrame(result_array, columns=list_columns+["Training Score", "Testing Score"])
+    
+    df_result_grouped_did_estimator = df_result.groupby(["Dataset ID"])
+    for name, group in df_result_grouped_did_estimator:
+        df_result_grouped = group.groupby(list_columns, as_index=False).agg(
+            {"Training Score": ["mean"], "Testing Score": ["mean"]})
+        df_result_grouped_ordered = df_result_grouped.sort_values(by=["Dataset ID", "Estimator", "Metric", "Learning Method", "Kernel", "Discretization Method", "Acquisition Function", "Number Features"], ascending=True) 
+        df_result_grouped_ordered.to_csv(
+            "results/iteration_number_experiment/bay_opt_iterations_" + str(name) + ".csv", index=False)
 
 if __name__ == "__main__":
     experiment_bayesian_iter_performance()
