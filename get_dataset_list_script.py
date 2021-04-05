11d37ddea2166912b8fd4d571606d8d2c53e852f
Patrick Ehrler <hallo@patrickehrler.de>
2021-04-05 17:50:21 +0200

    Update get datasets script

--- get_dataset_list_script.py
+++ get_dataset_list_script.py
@@ -5,7 +5,7 @@ def get_datasets(number_of_classes):
     dataset_overview = dataset_overview[
                 (dataset_overview['format'] != 'Sparse_ARFF') &  # would break when calling get_data()
                 (dataset_overview['NumberOfClasses'] >= number_of_classes) &
-                (dataset_overview['NumberOfNumericFeatures'] >= 200) &  # includes target variable
+                (dataset_overview['NumberOfNumericFeatures'] >= 102) &  # includes target variable
                 (dataset_overview['NumberOfNumericFeatures'] <= 10000) &
                 (dataset_overview['NumberOfInstances'] >= 200) &
                 (dataset_overview['NumberOfInstances'] <= 5000) &
@@ -14,8 +14,7 @@ def get_datasets(number_of_classes):
     return dataset_overview
 
 classification_binary_datasets = get_datasets(2)
-#regression_datasets = get_datasets(0)
 
-print(classification_binary_datasets)
+# print to csv
 classification_binary_datasets.to_csv("results/datasets/classification_binary_datasets.csv", index=False)
-#regression_datasets.to_csv("datasets/regression_datasets.csv", index=False)
+
